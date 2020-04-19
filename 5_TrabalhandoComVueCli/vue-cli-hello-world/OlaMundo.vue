e08b90f53ad9ded7d77ea55010fa4bcbac92efb8
Willian Dias Brito <willian.dias360@hotmail.com>
2020-04-19 15:03:49 -0300

    Trabalhando com VueCli

---
+++ 5_TrabalhandoComVueCli/vue-cli-hello-world/OlaMundo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{titulo}}</h1>
+        <input v-model="titulo">
+    </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      titulo: 'Olá VueJs'
+    }
+  }
+}
+</script>

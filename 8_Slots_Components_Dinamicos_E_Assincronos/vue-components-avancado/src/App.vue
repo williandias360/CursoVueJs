3b0c9cdb9ec25e5fdebd82b601f9bc942b6b86cd
willian.dias360@hotmail.com <Willian>
2020-05-03 16:25:14 -0300

    Slots, components dinamicos e assincronos

---
+++ 8_Slots_Components_Dinamicos_E_Assincronos/vue-components-avancado/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="container">
+    <h1>Vue JS</h1>
+    <Post>
+      <!-- Customizando os components
+      Cria um component como container. Pode passar qualquer html-->
+      <h2 slot="cabecalho">Components no Vue</h2>
+
+      <p>Components são umas das peças mais importantes no VueJs</p>
+      <span>...</span>
+
+      <small slot="rodape">por Willian Dias</small>
+
+      <!-- <template slot="botao">Detalhes</template> -->
+    </Post>
+  </div>
+</template>
+
+<script>
+import Post from "./components/Post.vue";
+export default {
+  components: {
+    Post
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 960px;
+  margin: auto;
+}
+</style>
+
+

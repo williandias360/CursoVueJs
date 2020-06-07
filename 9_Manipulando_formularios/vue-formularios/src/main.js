5d661f4b9a791c4000b4c7821c82b645c72108b8
Willian Dias <willian.dias360@hotmail.com>
2020-06-07 16:18:07 -0300

    Manipulando formularios inicio

---
+++ 9_Manipulando_formularios/vue-formularios/src/main.js
@@ -0,0 +1,14 @@
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import Vue from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')

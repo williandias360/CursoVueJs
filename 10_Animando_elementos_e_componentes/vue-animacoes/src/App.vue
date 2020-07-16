c41e836697751cec4fe647ca8644f9fa0473fd11
Willian Dias <willian.dias360@hotmail.com>
2020-07-16 11:45:45 -0300

    Animando elementos e componentes

---
+++ 10_Animando_elementos_e_componentes/vue-animacoes/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p class="lead">Treinando transiçoes/animações de elementos/componentes no Vue.</p>
+      </div>
+    </div>
+    <div class="container">
+      <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button>
+      <transition name="slide" type="animation">
+        <div class="alert alert-primary" v-if="mostrar">Animações no Vue</div>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mostrar: true
+    };
+  }
+};
+</script>
+
+<style scoped>
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-active {
+  animation: slide 0.7s;
+  transition: opacity 0.7s;
+}
+
+.slide-leave-active {
+  animation: slide 0.7s reverse;
+  transition: opacity 2s;
+}
+
+@keyframes slide {
+  0% {
+    transform: translateX(-100px);
+  }
+  100% {
+    transform: translateX(0px);
+  }
+}
+
+.zoom-enter,
+.zoom-leave-to {
+  transform: scale(0);
+}
+
+.zoom-enter-active,
+.zoom-leave-active {
+  transition: transform 0.5s;
+}
+
+.zoom-enter-to,
+.zoom-leave {
+  transform: scale(1);
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
+</style>
+
---
+++ 10_Animando_elementos_e_componentes/vue-animacoes/src/main.js
@@ -0,0 +1,14 @@
+import BootStrap from 'bootstrap-vue/dist/bootstrap-vue.esm'
+import Vue from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootStrap)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')

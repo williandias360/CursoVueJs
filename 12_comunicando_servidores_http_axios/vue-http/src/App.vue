3b368f5731081ea44facacb2fc047373f19e894d
Willian Dias <willian.dias360@hotmail.com>
2020-08-23 15:50:15 -0300

    Iniciando a sessão

---
+++ 12_comunicando_servidores_http_axios/vue-http/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Requisições HTTP no Vue</h1>
+        <p class="lead">Usando a biblioteca Axios para fazer chamadas Ajax à uma API REST.</p>
+      </div>
+    </div>
+
+    <div class="container"></div>
+  </div>
+</template>
---
+++ 12_comunicando_servidores_http_axios/vue-http/src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import BootStrap from "bootstrap-vue/dist/bootstrap-vue.esm";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootStrap);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount("#app");

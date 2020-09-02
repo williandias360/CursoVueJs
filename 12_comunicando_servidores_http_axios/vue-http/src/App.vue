6bcf981df821c9342956a4b368bf4a1b0bc6615c
Willian Dias <willian.dias360@hotmail.com>
2020-09-02 13:40:39 -0300

    Finalizando a sessao

--- 12_comunicando_servidores_http_axios/vue-http/src/App.vue
+++ 12_comunicando_servidores_http_axios/vue-http/src/App.vue
@@ -15,6 +15,21 @@
 
     <div class="container">
       <TarefasLista />
+
+      <button class="btn btn-primary mt-4 mb-2" @click="download">Baixar Imagem</button>
+      <div class="progress">
+        <div
+          class="progress-bar"
+          role="progressbar"
+          :style="{ width: progresso + '%'}"
+          :aria-valuenow="progresso"
+          aria-valuemin="0"
+          aria-valuemax="100"
+        >{{progresso}} %</div>
+      </div>
+      <div v-if="imagem">
+        <img :src="imagem" style="max-width:100%" />
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +42,12 @@ import TarefasLista from "./components/TarefasLista";
 export default {
   components: {
     TarefasLista,
+  },
+  data() {
+    return {
+      progresso: 0,
+      imagem: undefined,
+    };
   },
   async created() {
     //https://showmethecode.com.br/async-await/
@@ -63,6 +84,32 @@ export default {
     console.log("Requisições simultaneas:");
     console.log("Tarefa 1", tarefa1);
     console.log("Tarefa 3", tarefa3);
+  },
+  methods: {
+    download() {
+      axios
+        .get(
+          "https://images.unsplash.com/photo-1559301011-32201d61ba29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1008&q=80",
+          {
+            responseType: "blob",
+            onDownloadProgress: (progressEvent) => {
+              console.log("Fazendo download...", progressEvent);
+              this.progresso = (
+                (progressEvent.loaded / progressEvent.total) *
+                100
+              ).toFixed(0);
+            },
+          }
+        )
+        .then((response) => {
+          console.log("Download concluído!", response);
+          const reader = new window.FileReader();
+          reader.readAsDataURL(response.data);
+          reader.onload = () => {
+            this.imagem = reader.result;
+          };
+        });
+    },
   },
 };
 </script>

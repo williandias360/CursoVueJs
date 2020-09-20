bebf60c34cb8bbfc81d4ed5fee63bf7c1c564882
Willian Dias <willian.dias360@hotmail.com>
2020-09-20 15:43:48 -0300

    Criação do servidor GraphQL

---
+++ 14_Projeto_Final_Financaca_pessoais/vue-finances-back/index.js
@@ -0,0 +1,30 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+
+const resolvers = {
+  Query: {
+    user(parent, args, context, info) {
+      return prisma.user({ id: args.id });
+    },
+  },
+  /*Resolver Trivial
+  User: {
+    id(parent, args, context, info) {
+      console.log("Id: ", parent.id);
+      return parent.id;
+    },
+    name(parent, args, context, info) {
+      console.log("Name: ", parent.name);
+      return parent.name;
+    },
+  },*/
+};
+
+const server = new GraphQLServer({
+  typeDefs: "./schema.graphql",
+  resolvers,
+});
+
+server
+  .start()
+  .then(() => console.log("Server running on http://localhost:4000..."));
